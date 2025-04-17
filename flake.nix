76951b1647ec6c3a0a7d738aba5ba114e59334d3
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-04-17 23:12:52 +0900

    initila commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Interactive CG";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            fltk
+            gcc
+            cmake
+            doxygen
+            pkg-config
+            libGL
+            libGLU
+          ];
+        };
+      }
+    );
+}
